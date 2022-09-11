f3a7f4d67bfd3d56140bbc46aa4b22a92c69bdab
Himanshu Goswami <himgos@gmail.com>
2022-09-11 18:33:52 +0530

    Fixed html and removed download functionality

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import os
 from waitress import serve
 import logging as lg
 from flask_cors import CORS, cross_origin
+from pytube import YouTube
 
 
 # -- Configuring Logger --
@@ -256,31 +257,14 @@ def download(video_id):
 
     try:
 
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--no-sandbox")
-        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+        # -- This functionality doesn't work on webservers --
 
-        # driver = webdriver.Chrome()
-        # Heading to website from where we can download video
-        driver.get("https://en.savefrom.net/210/")
-        element = driver.find_element(By.ID, "sf_url")
-        # send url of video
-        element.send_keys("https://www.youtube.com/watch?v=" + video_id)
-        # submit button click
-        element.submit()
-
-        time.sleep(4)
-
-        # clicking the href of download button.
-        element1 = driver.find_element(By.LINK_TEXT, "Download")
-        # Finally, click on it.
-        element1.click()
+        # path = os.path.join(os.environ["HOMEPATH"], "Desktop")
+        # yt = YouTube("https://www.youtube.com/watch?v=TpFztC39j-U")
+        # yt.streams.get_lowest_resolution().download(path)
 
         lg.info("Successfully downloaded video in download function")
-        return render_template("download.html", video_id=video_id), 200
+        return render_template("download.html", video_id=video_id)
 
     except Exception as err:
         lg.error("download function didn't work")
